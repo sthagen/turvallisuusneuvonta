9a50aa4a03c22c6ec5f3fa023c43a69cc65d4e45
Stefan Hagen <stefan@hagen.link>
2021-12-08 18:08:16 +0100

    Added initial app boilerplate (WIP)

--- turvallisuusneuvonta/cli.py
+++ turvallisuusneuvonta/cli.py
@@ -1,1 +1,74 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# pylint: disable=line-too-long
+"""Commandline API gateway for turvallisuusneuvonta."""
+import pathlib
+import sys
+from typing import List, Union
+
+import typer
+
+import turvallisuusneuvonta
+import turvallisuusneuvonta.turvallisuusneuvonta as tu
+
+APP_NAME = 'Security advisory (Finnish: turvallisuusneuvonta) audit tool.'
+APP_ALIAS = 'turvallisuusneuvonta'
+app = typer.Typer(
+    add_completion=False,
+    context_settings={'help_option_names': ['-h', '--help']},
+    no_args_is_help=True,
+)
+
+
+@app.callback(invoke_without_command=True)
+def callback(
+    version: bool = typer.Option(
+        False,
+        '-V',
+        '--version',
+        help='Display the turvallisuusneuvonta version and exit',
+        is_eager=True,
+    )
+) -> None:
+    """
+    Security advisory (Finnish: turvallisuusneuvonta) audit tool.
+    """
+    if version:
+        typer.echo(f'{APP_NAME} version {turvallisuusneuvonta.__version__}')
+        raise typer.Exit()
+
+
+@app.command('verify')
+def verify(
+    conf: str = typer.Option(
+        '',
+        '-c',
+        '--config',
+        help='Path to config file (default is $HOME/.turvallisuusneuvonta.json)',
+        metavar='<configpath>',
+    ),
+) -> int:
+    """
+    Answer the question if now is a working hour.
+    """
+    command = 'verify'
+    config = conf if conf else pathlib.Path.home() / tu.DEFAULT_CONFIG_NAME
+    action = [command, str(config)]
+    return sys.exit(tu.main(action))
+
+
+@app.command('version')
+def app_version() -> None:
+    """
+    Display the turvallisuusneuvonta version and exit
+    """
+    callback(True)
+
+
+# pylint: disable=expression-not-assigned
+# @app.command()
+def main(argv: Union[List[str], None] = None) -> int:
+    """Delegate processing to functional module."""
+    argv = sys.argv[1:] if argv is None else argv
+    return tu.main(argv)
 
--- turvallisuusneuvonta/turvallisuusneuvonta.py
+++ turvallisuusneuvonta/turvallisuusneuvonta.py
@@ -1,1 +1,83 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=expression-not-assigned,line-too-long
+"""Working hours (Danish arbejdstimer) or not? API."""
+import datetime as dti
+import json
+import os
+import pathlib
+import sys
+from typing import List, Optional, Tuple, Union
+
+DEBUG_VAR = 'TURVALLISUUSNEUVONTA_DEBUG'
+DEBUG = os.getenv(DEBUG_VAR)
+
+ENCODING = 'utf-8'
+ENCODING_ERRORS_POLICY = 'ignore'
+
+
+DEFAULT_CONFIG_NAME = '.turvallisuusneuvonta.json'
+
+
+def weekday() -> int:
+    """Return current weekday."""
+    return dti.date.today().isoweekday()
+
+
+def no_weekend(day_number: int) -> bool:
+    """Return if day number is weekend."""
+    return day_number < 6
+
+
+def verify_request(argv: Optional[List[str]]) -> Tuple[int, str, List[str]]:
+    """Fail with grace."""
+    if not argv or len(argv) != 2:
+        return 2, 'received wrong number of arguments', ['']
+
+    command, config = argv
+
+    if command not in ('verify'):
+        return 2, 'received unknown command', ['']
+
+    if not config:
+        return 2, 'configuration missing', ['']
+
+    config_path = pathlib.Path(str(config))
+    if not config_path.is_file():
+        return 1, f'config ({config_path}) is no file', ['']
+    if not ''.join(config_path.suffixes).lower().endswith('.json'):
+        return 1, 'config has not .json extension', ['']
+
+    return 0, '', argv
+
+
+def main(argv: Union[List[str], None] = None) -> int:
+    """Drive the lookup."""
+    error, message, strings = verify_request(argv)
+    if error:
+        print(message, file=sys.stderr)
+        return error
+
+    command, config = strings
+
+    with open(config, 'rt', encoding=ENCODING) as handle:
+        configuration = json.load(handle)
+
+    print(f'configuration is ({configuration})')
+    week_day = weekday()
+    work_day = no_weekend(week_day)
+    if work_day:
+        print(f'Today ({dti.date.today()}) is not a weekend')
+    else:
+        print('Today is weekend.')
+        return 1
+
+    hour = dti.datetime.now().hour
+    if 6 < hour < 17:
+        print(f'At this hour ({hour}) is work time')
+    else:
+        print(f'No worktime at hour({hour}).')
+        return 1
+
+
+    return 0
 
