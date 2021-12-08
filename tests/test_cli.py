8eaec3e169977b262117d224d1fce7f140e39195
Stefan Hagen <stefan@hagen.link>
2021-12-08 18:04:30 +0100

    Initial tests (WIP)

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,1 +1,47 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=line-too-long,missing-docstring,reimported,unused-import,unused-variable
+import pathlib
+
+import click
+import pytest
+
+import turvallisuusneuvonta.cli as cli
+
+
+def test_main_legacy_ok(capsys):
+    inp = str(pathlib.Path('tests', 'fixtures', 'empty', 'advisory.json'))
+    assert cli.main(['verify', inp]) == 0
+    out, err = capsys.readouterr()
+    assert 'whatever is' in out.lower()
+    assert not err
+
+
+def test_version_ok(capsys):
+    with pytest.raises(click.exceptions.Exit) as exec_info:
+        cli.app_version() == 0
+        assert exec_info.value.code == 0
+        out, err = capsys.readouterr()
+        assert 'version' in out.lower()
+        assert not err
+
+
+def test_now_ok(capsys):
+    in_path = pathlib.Path('tests', 'fixtures', 'empty', 'advisory.json')
+    with pytest.raises(SystemExit) as exec_info:
+        cli.now(conf=in_path)
+        assert exec_info.value.code == 0
+        out, err = capsys.readouterr()
+        assert 'would verify advisory' in out.lower()
+        assert not err
+
+
+def test_translate_non_existing_html(capsys):
+    in_path = pathlib.Path('does', 'not', 'exist', 'hypothetical.json')
+    with pytest.raises(SystemExit) as exec_info:
+        cli.now(conf=in_path)
+        assert exec_info.value.code == 1
+        out, err = capsys.readouterr()
+        assert 'source' in out.lower()
+        assert 'is no file' in out.lower()
+        assert not err
 
--- tests/test_turvallisuusneuvonta.py
+++ tests/test_turvallisuusneuvonta.py
@@ -1,1 +1,24 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=line-too-long,missing-docstring,reimported,unused-import,unused-variable
+import pathlib
+
+import turvallisuusneuvonta.turvallisuusneuvonta as tu
+
+
+def test_tu_main():
+    inp = str(pathlib.Path('tests', 'fixtures', 'empty', 'advisory.json'))
+    assert tu.main(['verify', inp]) == 0
+
+
+def test_tu_verify_request_too_few():
+    assert at.verify_request([1]) == (2, 'received wrong number of arguments', [''])
+
+
+def test_tu_verify_request_unknown_command():
+    assert tu.verify_request(['unknown', 'does not matter']) == (2, 'received unknown command', [''])
+
+
+def test_tu_verify_request_falsy_input():
+    argv = ['verify', '']
+    assert tu.verify_request(argv) == (2, 'input missing', [''])
 
