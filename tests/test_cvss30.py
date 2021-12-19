445856dbf6c2216db12caaac9b36bea1181553d4
Stefan Hagen <stefan@hagen.link>
2021-12-19 13:46:09 +0100

    Added detailed positive test for log4j CVE and CVSS 3.0

--- tests/test_cvss30.py
+++ tests/test_cvss30.py
@@ -7,7 +7,7 @@ import turvallisuusneuvonta.csaf.cvss30.cvss30 as cvss30
 
 CVSS30_VECTOR_STRING_LOG4J = 'CVSS:3.0/AV:N/AC:L/PR:N/UI:N/S:C/C:H/I:H/A:H'
 CVSS30_BASE_SCORE_LOG4J = '10.0'
-CVSS30_BASE_SEVERITY_LOG4J = 'CRITICAL'
+CVSS30_BASE_SEVERITY_LOG4J = cvss30.SeverityType.critical
 
 
 def test_cvss30_empty():
@@ -27,3 +27,19 @@ def test_cvss30_wrong_version():
     with pytest.raises(ValidationError, match=message) as err:
         _ = cvss30.CVSS(**data)
     assert '\nversion\n  value is not a valid enumeration member' in str(err.value)
+
+
+def test_cvss30_log4j_cve_2021_44228():
+    data = {
+        'version': cvss30.Version.value,
+        'vectorString': CVSS30_VECTOR_STRING_LOG4J,
+        'baseScore': CVSS30_BASE_SCORE_LOG4J,
+        'baseSeverity': CVSS30_BASE_SEVERITY_LOG4J,
+    }
+    cvss_cve_2021_44228 = cvss30.CVSS(**data)
+    assert isinstance(cvss_cve_2021_44228, cvss30.CVSS)
+    assert cvss_cve_2021_44228.version == cvss30.Version.value
+    assert cvss_cve_2021_44228.vector_string == CVSS30_VECTOR_STRING_LOG4J
+    assert cvss_cve_2021_44228.base_score.__root__ == float(CVSS30_BASE_SCORE_LOG4J)
+    assert cvss_cve_2021_44228.base_severity.critical == CVSS30_BASE_SEVERITY_LOG4J
+    assert cvss_cve_2021_44228.confidentiality_requirement is None
