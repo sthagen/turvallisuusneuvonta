69bc4c028e54d230bf1783cd0d0ddcda43c485c5
Stefan Hagen <stefan@hagen.link>
2021-12-19 14:01:28 +0100

    Added another CLI test for branch coverage and dried up some of the usual CLI test swamp

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -16,19 +16,25 @@ def test_main_legacy_ok(capsys):
     assert 'configuration missing' in err.lower()
 
 
+def test_callback_version_false(capsys):
+    assert cli.callback(version=False) is None
+    out, err = capsys.readouterr()
+    assert not out
+    assert not err.lower()
+
+
 def test_version_ok(capsys):
-    with pytest.raises(click.exceptions.Exit) as exec_info:
-        cli.app_version() == 0
-        assert exec_info.value.code == 0
-        out, err = capsys.readouterr()
-        assert 'version' in out.lower()
-        assert not err
+    with pytest.raises(click.exceptions.Exit):
+        cli.app_version()
+    out, err = capsys.readouterr()
+    assert 'version' in out.lower()
+    assert not err
 
 
 def test_now_ok(capsys):
     in_path = pathlib.Path('tests', 'fixtures', 'empty', 'advisory.json')
     with pytest.raises(SystemExit) as exec_info:
-        cli.verify(conf=in_path)
+        cli.verify(conf=str(in_path))
         assert exec_info.value.code == 0
         out, err = capsys.readouterr()
         assert 'would verify advisory' in out.lower()
@@ -38,7 +44,7 @@ def test_now_ok(capsys):
 def test_translate_non_existing_html(capsys):
     in_path = pathlib.Path('does', 'not', 'exist', 'hypothetical.json')
     with pytest.raises(SystemExit) as exec_info:
-        cli.verify(conf=in_path)
+        cli.verify(conf=str(in_path))
         assert exec_info.value.code == 1
         out, err = capsys.readouterr()
         assert 'source' in out.lower()
