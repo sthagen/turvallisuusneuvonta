094ea7916f891a80ce6880032cc7e966b6d55fd8
Stefan Hagen <stefan@hagen.link>
2021-12-19 12:59:37 +0100

    Added CVSS 3.0 test seed (WIP)

---
+++ tests/test_cvss30.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=line-too-long,missing-docstring,reimported,unused-import,unused-variable
+import pytest
+from pydantic.error_wrappers import ValidationError
+
+import turvallisuusneuvonta.csaf.cvss30.cvss30 as cvss30
+
+
+def test_cvss2_empty():
+    message = '4 validation errors for Field0'
+    with pytest.raises(ValidationError, match=message):
+        _ = cvss30.Field0()
