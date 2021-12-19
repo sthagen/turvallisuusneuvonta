24dc308b495261162bc5a899cde7d06a62dfc824
Stefan Hagen <stefan@hagen.link>
2021-12-19 13:23:52 +0100

    Added wrong version test for CVSS2

--- tests/test_cvss2.py
+++ tests/test_cvss2.py
@@ -5,8 +5,18 @@ from pydantic.error_wrappers import ValidationError
 
 import turvallisuusneuvonta.csaf.cvss2.cvss2 as cvss2
 
+CVSS2_VECTOR_STRING_LOG4J = 'AV:N/AC:M/Au:N/C:C/I:C/A:C'
+CVSS2_BASE_SCORE_LOG4J = '10.0'
+
 
 def test_cvss2_empty():
     message = '3 validation errors for CVSS'
     with pytest.raises(ValidationError, match=message):
         _ = cvss2.CVSS()
+
+
+def test_cvss2_wrong_version():
+    message = '1 validation error for CVSS'
+    with pytest.raises(ValidationError, match=message) as err:
+        _ = cvss2.CVSS(version='42', vectorString=CVSS2_VECTOR_STRING_LOG4J, baseScore=CVSS2_BASE_SCORE_LOG4J)
+    assert '\nversion\n  value is not a valid enumeration member' in str(err.value)
