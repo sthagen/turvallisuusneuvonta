fdfbb4d6265a47f56d21d26c4b9fb18037e9e4b0
Stefan Hagen <stefan@hagen.link>
2021-12-20 15:49:42 +0100

    Renamed and extended single meta document test

--- tests/test_csaf_meta.py
+++ tests/test_csaf_meta.py
@@ -6,7 +6,9 @@ from pydantic.error_wrappers import ValidationError
 import turvallisuusneuvonta.csaf.document as document
 
 
-def test_meta_doc_empty():
+def test_meta_doc_none():
     message = '5 validation errors for DocumentLevelMetaData'
-    with pytest.raises(ValidationError, match=message):
+    with pytest.raises(ValidationError, match=message) as err:
         _ = document.DocumentLevelMetaData()  # type: ignore
+    for prop in ('category', 'csaf_version', 'publisher', 'title', 'tracking'):
+        assert f'\n{prop}\n  field required' in str(err.value)
