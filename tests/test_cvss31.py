0f73fc893a126d0f3e08ab18e0d7cf670c1235cb
Stefan Hagen <stefan@hagen.link>
2021-12-20 12:49:18 +0100

    Specialized class name for  CVSS 3.1 (WIP)

--- tests/test_cvss31.py
+++ tests/test_cvss31.py
@@ -11,33 +11,33 @@ CVSS31_BASE_SEVERITY_LOG4J = cvss31.SeverityType.critical
 
 
 def test_cvss31_empty():
-    message = '4 validation errors for CVSS'
+    message = '4 validation errors for CVSS31'
     with pytest.raises(ValidationError, match=message):
-        _ = cvss31.CVSS()
+        _ = cvss31.CVSS31()
 
 
 def test_cvss31_wrong_version():
     data = {
         'version': '42',
-        'vectorString': CVSS31_VECTOR_STRING_LOG4J,
-        'baseScore': CVSS31_BASE_SCORE_LOG4J,
-        'baseSeverity': CVSS31_BASE_SEVERITY_LOG4J,
+        'vector_string': CVSS31_VECTOR_STRING_LOG4J,
+        'base_score': CVSS31_BASE_SCORE_LOG4J,
+        'base_severity': CVSS31_BASE_SEVERITY_LOG4J,
     }
-    message = '1 validation error for CVSS'
+    message = '1 validation error for CVSS31'
     with pytest.raises(ValidationError, match=message) as err:
-        _ = cvss31.CVSS(**data)
+        _ = cvss31.CVSS31(**data)
     assert '\nversion\n  value is not a valid enumeration member' in str(err.value)
 
 
 def test_cvss31_log4j_cve_2021_44228():
     data = {
         'version': cvss31.Version.value,
-        'vectorString': CVSS31_VECTOR_STRING_LOG4J,
-        'baseScore': CVSS31_BASE_SCORE_LOG4J,
-        'baseSeverity': CVSS31_BASE_SEVERITY_LOG4J,
+        'vector_string': CVSS31_VECTOR_STRING_LOG4J,
+        'base_score': CVSS31_BASE_SCORE_LOG4J,
+        'base_severity': CVSS31_BASE_SEVERITY_LOG4J,
     }
-    cvss_cve_2021_44228 = cvss31.CVSS(**data)
-    assert isinstance(cvss_cve_2021_44228, cvss31.CVSS)
+    cvss_cve_2021_44228 = cvss31.CVSS31(**data)
+    assert isinstance(cvss_cve_2021_44228, cvss31.CVSS31)
     assert cvss_cve_2021_44228.version == cvss31.Version.value
     assert cvss_cve_2021_44228.vector_string == CVSS31_VECTOR_STRING_LOG4J
     assert cvss_cve_2021_44228.base_score.__root__ == float(CVSS31_BASE_SCORE_LOG4J)
