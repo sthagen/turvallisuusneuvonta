2102d72dc942fc513167dc6a86f45c35bd7eb321
Stefan Hagen <stefan@hagen.link>
2021-12-20 15:57:42 +0100

    Added document meta tet with wrong CSAF version

--- tests/test_csaf_meta.py
+++ tests/test_csaf_meta.py
@@ -5,6 +5,11 @@ from pydantic.error_wrappers import ValidationError
 
 import turvallisuusneuvonta.csaf.document as document
 
+META_WRONG_VERSION = {
+    'category': '42',
+    'csaf_version': '42',
+}
+
 
 def test_meta_doc_none():
     message = '5 validation errors for DocumentLevelMetaData'
@@ -21,4 +26,14 @@ def test_meta_doc_category_empty():
     hint = 'ensure this value has at least 1 character'
     assert f'\ncategory\n  {hint}' in str(err.value)
     for prop in ('csaf_version', 'publisher', 'title', 'tracking'):
+        assert f'\n{prop}\n  field required' in str(err.value)
+
+
+def test_meta_doc_csaf_version():
+    message = '4 validation errors for DocumentLevelMetaData'
+    with pytest.raises(ValidationError, match=message) as err:
+        _ = document.DocumentLevelMetaData(**META_WRONG_VERSION)  # type: ignore
+    hint = "value is not a valid enumeration member; permitted: '2.0'"
+    assert f'\ncsaf_version\n  {hint}' in str(err.value)
+    for prop in ('publisher', 'title', 'tracking'):
         assert f'\n{prop}\n  field required' in str(err.value)
