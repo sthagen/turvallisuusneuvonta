91711c4c14447c5418b14a6bfad895da8f71b7e8
Stefan Hagen <stefan@hagen.link>
2021-12-30 21:51:50 +0100

    New patched up document section of model (WIP)

--- turvallisuusneuvonta/csaf/document.py
+++ turvallisuusneuvonta/csaf/document.py
@@ -1,20 +1,13 @@
 from __future__ import annotations
 
 """CSAF Document meta information model."""
-from collections.abc import Sequence
 from datetime import datetime
 from enum import Enum
-from typing import Annotated, Optional, no_type_check
+from typing import Annotated, List, Optional, no_type_check
 
 from pydantic import AnyUrl, BaseModel, Field, validator
 
-from turvallisuusneuvonta.csaf.definitions import (
-    LanguageType,
-    ListOfAcknowledgments,
-    ListOfNotes,
-    ListOfReferences,
-    Version,
-)
+from turvallisuusneuvonta.csaf.definitions import Acknowledgments, Lang, Notes, References, Version
 
 
 class Revision(BaseModel):
@@ -44,13 +37,13 @@ class Tracking(BaseModel):
     """
 
     aliases: Annotated[
-        Optional[Sequence[AlternateName]],
+        Optional[List[Alias]],
         Field(
             description='Contains a list of alternate names for the same document.',
             min_items=1,
             title='Aliases',
         ),
-    ] = None
+    ]
     current_release_date: Annotated[
         datetime,
         Field(
@@ -59,23 +52,20 @@ class Tracking(BaseModel):
         ),
     ]
     generator: Annotated[
-        Optional[DocumentGenerator],
+        Optional[Generator],
         Field(
-            description=(
-                'Is a container to hold all elements related to the generation of the document.'
-                ' These items will reference when the document was actually created,'
-                ' including the date it was generated and the entity that generated it.'
-            ),
+            description='Is a container to hold all elements related to the generation of the document.'
+            ' These items will reference when the document was actually created,'
+            ' including the date it was generated and the entity that generated it.',
             title='Document generator',
         ),
-    ] = None
+    ]
     id: Annotated[
         str,
         Field(
-            description=(
-                'The ID is a simple label that provides for a wide range of numbering values, types, and schemes.'
-                ' Its value SHOULD be assigned and maintained by the original document issuing authority.'
-            ),
+            description='The ID is a simple label that provides for a wide range of numbering values, types,'
+            ' and schemes. Its value SHOULD be assigned and maintained by the original document'
+            ' issuing authority.',
             examples=[
                 'Example Company - 2019-YH3234',
                 'RHBA-2019:0024',
@@ -93,10 +83,10 @@ class Tracking(BaseModel):
         ),
     ]
     revision_history: Annotated[
-        Sequence[Revision],
+        List[Revision],
         Field(
             description='Holds one revision item for each version of the CSAF document, including the initial one.',
-            # min_items=1,
+            min_items=1,
             title='Revision history',
         ),
     ]
@@ -120,9 +110,9 @@ class Tracking(BaseModel):
 class AggregateSeverity(BaseModel):
     """
     Is a vehicle that is provided by the document producer to convey the urgency and criticality with which the one or
-    more vulnerabilities reported should be addressed.
-    It is a document-level metric and applied to the document as a whole — not any specific vulnerability.
-    The range of values in this field is defined according to the document producer's policies and procedures.
+    more vulnerabilities reported should be addressed. It is a document-level metric and applied to the document as a
+    whole — not any specific vulnerability. The range of values in this field is defined according to the document
+    producer's policies and procedures.
     """
 
     namespace: Annotated[
@@ -131,14 +121,12 @@ class AggregateSeverity(BaseModel):
             description='Points to the namespace so referenced.',
             title='Namespace of aggregate severity',
         ),
-    ] = None
+    ]
     text: Annotated[
         str,
         Field(
-            description=(
-                'Provides a severity which is independent of - and in addition to - any other standard metric for'
-                ' determining the impact or severity of a given vulnerability (such as CVSS).'
-            ),
+            description='Provides a severity which is independent of - and in addition to - any other standard metric'
+            ' for determining the impact or severity of a given vulnerability (such as CVSS).',
             examples=['Critical', 'Important', 'Moderate'],
             min_length=1,
             title='Text of aggregate severity',
@@ -151,46 +139,44 @@ class CsafVersion(Enum):
     Gives the version of the CSAF specification which the document was generated for.
     """
 
-    field_2_0 = '2.0'
+    version_2_0 = '2.0'
 
 
-class LabelOfTlp(Enum):
+class Label(Enum):
     """
     Provides the TLP label of the document.
     """
 
-    amber = 'AMBER'
-    green = 'GREEN'
-    red = 'RED'
-    white = 'WHITE'
+    AMBER = 'AMBER'
+    GREEN = 'GREEN'
+    RED = 'RED'
+    WHITE = 'WHITE'
 
 
-class TrafficLightProtocolTlp(BaseModel):
+class TrafficLightProtocol(BaseModel):
     """
     Provides details about the TLP classification of the document.
     """
 
     label: Annotated[
-        LabelOfTlp,
+        Label,
         Field(description='Provides the TLP label of the document.', title='Label of TLP'),
     ]
     url: Annotated[
         Optional[AnyUrl],
         Field(
-            description=(
-                'Provides a URL where to find the textual description of the TLP version which is used in this'
-                ' document. Default is the URL to the definition by FIRST.'
-            ),
+            description='Provides a URL where to find the textual description of the TLP version which is used in this'
+            ' document. Default is the URL to the definition by FIRST.',
             examples=[
                 'https://www.us-cert.gov/tlp',
                 'https://www.bsi.bund.de/SharedDocs/Downloads/DE/BSI/Kritis/Merkblatt_TLP.pdf',
             ],
             title='URL of TLP version',
         ),
-    ] = 'https://www.first.org/tlp/'  # type: ignore
+    ] = AnyUrl(url='https://www.first.org/tlp/', host='www.first.org', scheme='https')
 
 
-class RulesForSharingDocument(BaseModel):
+class Distribution(BaseModel):
     """
     Describe any constraints on how this document might be shared.
     """
@@ -207,17 +193,17 @@ class RulesForSharingDocument(BaseModel):
             min_length=1,
             title='Textual description',
         ),
-    ] = None
+    ]
     tlp: Annotated[
-        Optional[TrafficLightProtocolTlp],
+        Optional[TrafficLightProtocol],
         Field(
             description='Provides details about the TLP classification of the document.',
             title='Traffic Light Protocol (TLP)',
         ),
-    ] = None
+    ]
 
 
-class CategoryOfPublisher(Enum):
+class PublisherCategory(Enum):
     """
     Provides information about the category of publisher releasing the document.
     """
@@ -236,7 +222,7 @@ class Publisher(BaseModel):
     """
 
     category: Annotated[
-        CategoryOfPublisher,
+        PublisherCategory,
         Field(
             description='Provides information about the category of publisher releasing the document.',
             title='Category of publisher',
@@ -245,31 +231,25 @@ class Publisher(BaseModel):
     contact_details: Annotated[
         Optional[str],
         Field(
-            description=(
-                'Information on how to contact the publisher, possibly including details such as web sites,'
-                ' email addresses, phone numbers, and postal mail addresses.'
-            ),
+            description='Information on how to contact the publisher, possibly including details such as web sites,'
+            ' email addresses, phone numbers, and postal mail addresses.',
             examples=[
-                (
-                    'Example Company can be reached at contact_us@example.com,'
-                    ' or via our website at https://www.example.com/contact.'
-                )
+                'Example Company can be reached at contact_us@example.com, or via our website'
+                ' at https://www.example.com/contact.'
             ],
             min_length=1,
             title='Contact details',
         ),
-    ] = None
+    ]
     issuing_authority: Annotated[
         Optional[str],
         Field(
-            description=(
-                'Provides information about the authority of the issuing party to release the document,'
-                " in particular, the party's constituency and responsibilities or other obligations."
-            ),
+            description='Provides information about the authority of the issuing party to release the document,'
+            " in particular, the party's constituency and responsibilities or other obligations.",
             min_length=1,
             title='Issuing authority',
         ),
-    ] = None
+    ]
     name: Annotated[
         str,
         Field(
@@ -282,24 +262,20 @@ class Publisher(BaseModel):
     namespace: Annotated[
         AnyUrl,
         Field(
-            description=(
-                'Contains a URL which is under control of the issuing party and can be used as a'
-                ' globally unique identifier for that issuing party.'
-            ),
+            description='Contains a URL which is under control of the issuing party and can be used as a globally'
+            ' unique identifier for that issuing party.',
             examples=['https://csaf.io', 'https://www.example.com'],
             title='Namespace of publisher',
         ),
     ]
 
 
-class AlternateName(BaseModel):
+class Alias(BaseModel):
     __root__: Annotated[
         str,
         Field(
-            description=(
-                'Specifies a non-empty string that represents a distinct optional alternative ID'
-                ' used to refer to the document.'
-            ),
+            description='Specifies a non-empty string that represents a distinct optional alternative ID used to'
+            ' refer to the document.',
             examples=['CVE-2019-12345'],
             min_length=1,
             title='Alternate name',
@@ -307,7 +283,7 @@ class AlternateName(BaseModel):
     ]
 
 
-class EngineOfDocumentGeneration(BaseModel):
+class Engine(BaseModel):
     """
     Contains information about the engine that generated the CSAF document.
     """
@@ -329,30 +305,27 @@ class EngineOfDocumentGeneration(BaseModel):
             min_length=1,
             title='Engine version',
         ),
-    ] = None
+    ]
 
 
-class DocumentGenerator(BaseModel):
+class Generator(BaseModel):
     """
-    Is a container to hold all elements related to the generation of the document.
-    These items will reference when the document was actually created,
-    including the date it was generated and the entity that generated it.
+    Is a container to hold all elements related to the generation of the document. These items will reference when
+    the document was actually created, including the date it was generated and the entity that generated it.
     """
 
     date: Annotated[
         Optional[datetime],
         Field(
-            description=(
-                'This SHOULD be the current date that the document was generated.'
-                ' Because documents are often generated internally by a document producer and exist for'
-                ' a nonzero amount of time before being released,'
-                ' this field MAY be different from the Initial Release Date and Current Release Date.'
-            ),
+            description='This SHOULD be the current date that the document was generated. Because documents are'
+            ' often generated internally by a document producer and exist for a nonzero amount of time'
+            ' before being released, this field MAY be different from the Initial Release Date and'
+            ' Current Release Date.',
             title='Date of document generation',
         ),
-    ] = None
+    ]
     engine: Annotated[
-        EngineOfDocumentGeneration,
+        Engine,
         Field(
             description='Contains information about the engine that generated the CSAF document.',
             title='Engine of document generation',
@@ -370,39 +343,46 @@ class DocumentStatus(Enum):
     interim = 'interim'
 
 
-class DocumentLevelMetaData(BaseModel):
+class RelationshipCategory(Enum):
+    """
+    Defines the category of relationship for the referenced component.
+    """
+
+    default_component_of = 'default_component_of'
+    external_component_of = 'external_component_of'
+    installed_on = 'installed_on'
+    installed_with = 'installed_with'
+    optional_component_of = 'optional_component_of'
+
+
+class Document(BaseModel):
     """
     Captures the meta-data about this document describing a particular set of security advisories.
     """
 
     acknowledgments: Annotated[
-        Optional[ListOfAcknowledgments],
+        Optional[Acknowledgments],
         Field(
             description='Contains a list of acknowledgment elements associated with the whole document.',
             title='Document acknowledgments',
         ),
-    ] = None
+    ]
     aggregate_severity: Annotated[
         Optional[AggregateSeverity],
         Field(
-            description=(
-                'Is a vehicle that is provided by the document producer to convey the urgency and criticality with'
-                ' which the one or more vulnerabilities reported should be addressed.'
-                ' It is a document-level metric and applied to the document as a whole'
-                ' — not any specific vulnerability.'
-                ' The range of values in this field is defined according to'
-                " the document producer's policies and procedures."
-            ),
+            description='Is a vehicle that is provided by the document producer to convey the urgency and'
+            ' criticality with which the one or more vulnerabilities reported should be addressed.'
+            ' It is a document-level metric and applied to the document as a whole'
+            ' — not any specific vulnerability. The range of values in this field is defined according'
+            " to the document producer's policies and procedures.",
             title='Aggregate severity',
         ),
-    ] = None
+    ]
     category: Annotated[
         str,
         Field(
-            description=(
-                'Defines a short canonical name, chosen by the document producer,'
-                ' which will inform the end user as to the category of document.'
-            ),
+            description='Defines a short canonical name, chosen by the document producer, which will inform the end'
+            ' user as to the category of document.',
             examples=[
                 'Example Company Security Notice',
                 'generic_csaf',
@@ -421,26 +401,26 @@ class DocumentLevelMetaData(BaseModel):
         ),
     ]
     distribution: Annotated[
-        Optional[RulesForSharingDocument],
+        Optional[Distribution],
         Field(
             description='Describe any constraints on how this document might be shared.',
             title='Rules for sharing document',
         ),
-    ] = None
+    ]
     lang: Annotated[
-        Optional[LanguageType],
+        Optional[Lang],
         Field(
             description='Identifies the language used by this document, corresponding to IETF BCP 47 / RFC 5646.',
             title='Document language',
         ),
-    ] = None
+    ]
     notes: Annotated[
-        Optional[ListOfNotes],
+        Optional[Notes],
         Field(
             description='Holds notes associated with the whole document.',
             title='Document notes',
         ),
-    ] = None
+    ]
     publisher: Annotated[
         Publisher,
         Field(
@@ -449,29 +429,25 @@ class DocumentLevelMetaData(BaseModel):
         ),
     ]
     references: Annotated[
-        Optional[ListOfReferences],
+        Optional[References],
         Field(
             description='Holds a list of references associated with the whole document.',
             title='Document references',
         ),
-    ] = None
+    ]
     source_lang: Annotated[
-        Optional[LanguageType],
+        Optional[Lang],
         Field(
-            description=(
-                'If this copy of the document is a translation then the value of this property describes from'
-                ' which language this document was translated.'
-            ),
+            description='If this copy of the document is a translation then the value of this property describes'
+            ' from which language this document was translated.',
             title='Source language',
         ),
-    ] = None
+    ]
     title: Annotated[
         str,
         Field(
-            description=(
-                'This SHOULD be a canonical name for the document,'
-                ' and sufficiently unique to distinguish it from similar documents.'
-            ),
+            description='This SHOULD be a canonical name for the document, and sufficiently unique to distinguish'
+            ' it from similar documents.',
             examples=[
                 'Cisco IPv6 Crafted Packet Denial of Service Vulnerability',
                 'Example Company Cross-Site-Scripting Vulnerability in Example Generator',
@@ -483,10 +459,8 @@ class DocumentLevelMetaData(BaseModel):
     tracking: Annotated[
         Tracking,
         Field(
-            description=(
-                'Is a container designated to hold all management attributes necessary to track'
-                ' a CSAF document as a whole.'
-            ),
+            description='Is a container designated to hold all management attributes necessary to track a'
+            ' CSAF document as a whole.',
             title='Tracking',
         ),
     ]
