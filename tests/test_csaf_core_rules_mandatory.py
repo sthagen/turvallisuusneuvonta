0de9847df59ecba4dddb1fd1efb3f3f733a91bfb
Stefan Hagen <stefan@hagen.link>
2022-01-01 18:33:23 +0100

    Added another existence test

--- tests/test_csaf_core_rules_mandatory.py
+++ tests/test_csaf_core_rules_mandatory.py
@@ -22,3 +22,12 @@ def test_mandatory_exists_single_claim_multiple_paths():
         ('sartre', 'exists', True),
         ('sartre', 'also', True),
     )
+
+
+def test_mandatory_exists_single_claim_multiple_paths_mixed_results():
+    document = {'exists': 'truthy', 'also': False}
+    claims = {'sartre': ['exists', 'also']}
+    assert mandatory.exists(document, claims) == (
+        ('sartre', 'exists', True),
+        ('sartre', 'also', False),
+    )
