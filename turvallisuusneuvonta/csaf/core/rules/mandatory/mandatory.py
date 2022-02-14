4a8bef229dd7912f0f1e0938006d52ad9d12658a
Stefan Hagen <stefan@hagen.link>
2022-02-14 22:23:52 +0100

    Removed topical approach (for now) and cleansed some code

--- turvallisuusneuvonta/csaf/core/rules/mandatory/mandatory.py
+++ turvallisuusneuvonta/csaf/core/rules/mandatory/mandatory.py
@@ -2,27 +2,14 @@ from typing import Dict, List, Tuple, no_type_check
 
 import jmespath
 
-import turvallisuusneuvonta.csaf.core.rules.mandatory.acyclic_product_ids as acy_product_ids  # type: ignore
-import turvallisuusneuvonta.csaf.core.rules.mandatory.consistent_product_status as con_pro_sta  # type: ignore
-import turvallisuusneuvonta.csaf.core.rules.mandatory.defined_group_ids as def_gro_ids  # type: ignore
-import turvallisuusneuvonta.csaf.core.rules.mandatory.defined_product_ids as def_pro_ids  # type: ignore
-import turvallisuusneuvonta.csaf.core.rules.mandatory.translator_and_source_lang as tra_and_sou_lan  # type: ignore
-import turvallisuusneuvonta.csaf.core.rules.mandatory.unique_group_ids as uni_gro_ids  # type: ignore
-import turvallisuusneuvonta.csaf.core.rules.mandatory.unique_product_ids as uni_pro_ids  # type: ignore
-import turvallisuusneuvonta.csaf.core.rules.mandatory.valid_category_name as val_cat_nam  # type: ignore
-
-TOPICS = (
-    acy_product_ids,
-    con_pro_sta,
-    def_gro_ids,
-    def_pro_ids,
-    tra_and_sou_lan,
-    uni_gro_ids,
-    uni_pro_ids,
-    val_cat_nam,
-)
-
-TOPIC_MAP = {topic.TOPIC: topic.PATHS for topic in TOPICS}
+# import turvallisuusneuvonta.csaf.core.rules.mandatory.acyclic_product_ids as acy_product_ids
+# import turvallisuusneuvonta.csaf.core.rules.mandatory.consistent_product_status as con_pro_sta
+import turvallisuusneuvonta.csaf.core.rules.mandatory.defined_group_ids as def_gro_ids
+import turvallisuusneuvonta.csaf.core.rules.mandatory.defined_product_ids as def_pro_ids
+import turvallisuusneuvonta.csaf.core.rules.mandatory.translator_and_source_lang as tra_and_sou_lan
+import turvallisuusneuvonta.csaf.core.rules.mandatory.unique_group_ids as uni_gro_ids
+import turvallisuusneuvonta.csaf.core.rules.mandatory.unique_product_ids as uni_pro_ids
+import turvallisuusneuvonta.csaf.core.rules.mandatory.valid_category_name as val_cat_nam
 
 
 @no_type_check
