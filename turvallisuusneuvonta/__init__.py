9fbf64186729e56ec0c02959fb3262a530df7f57
Stefan Hagen <stefan@hagen.link>
2022-02-14 23:21:07 +0100

    Better programmatic usability

--- turvallisuusneuvonta/__init__.py
+++ turvallisuusneuvonta/__init__.py
@@ -1,2 +1,21 @@
 """Security advisory (Finnish: turvallisuusneuvonta) audit tool."""
 __version__ = '2022.2.14'
+from turvallisuusneuvonta.csaf.core.rules.mandatory.mandatory import (
+    is_valid,
+    is_valid_category,
+    is_valid_defined_group_ids,
+    is_valid_defined_product_ids,
+    is_valid_translator,
+    is_valid_unique_group_ids,
+    is_valid_unique_product_ids,
+)
+
+__all__ = [
+    'is_valid',
+    'is_valid_category',
+    'is_valid_defined_group_ids',
+    'is_valid_defined_product_ids',
+    'is_valid_translator',
+    'is_valid_unique_group_ids',
+    'is_valid_unique_product_ids',
+]
