6e14e621f41bc77008537dac1bdaf89bc252c058
Stefan Hagen <stefan@hagen.link>
2023-06-18 21:13:10 +0200

    Bumped impl version for release 2023.6.18

--- turvallisuusneuvonta/__init__.py
+++ turvallisuusneuvonta/__init__.py
@@ -1,7 +1,7 @@
 """Security advisory (Finnish: turvallisuusneuvonta) audit tool."""
 # [[[fill git_describe()]]]
-__version__ = '2022.8.9+parent.3233165e'
-# [[[end]]] (checksum: ec2cc12146ac51673531f0f6ebebef47)
+__version__ = '2023.6.18+parent.dd252054'
+# [[[end]]] (checksum: ed24f138a6c1f4ed1846f228247c7db7)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
