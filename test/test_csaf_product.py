722d28502b0251d1041ab7c7dcae98dd12064ad1
Stefan Hagen <stefan@hagen.link>
2023-08-21 15:00:17 +0200

    Fixed the 2 remaining broken product test by migrating the test data to the changed syntax from upstream

--- test/test_csaf_product.py
+++ test/test_csaf_product.py
@@ -39,15 +39,15 @@ def test_product_relationship():
 
 def test_product_relationship_loads():
     rel = product.Relationship(**conftest.PRODUCT_RELATIONSHIP_DATA)
-    assert msgspec.json.decode(rel.json()) == conftest.PRODUCT_RELATIONSHIP_DATA
+    assert msgspec.json.decode(rel.model_dump_json()) == conftest.PRODUCT_RELATIONSHIP_DATA
 
 
 def test_product_relationship_dumps():
-    pr_ref_other = product.ReferenceTokenForProductInstance(value='acme-101')
-    pr_ref_self = product.ReferenceTokenForProductInstance(value='acme-112')
+    pr_ref_other = product.ReferenceTokenForProductInstance('acme-101')
+    pr_ref_self = product.ReferenceTokenForProductInstance('acme-112')
     pr_id = pr_ref_self
-    pr_ids = product.Products(__root__=['acme-101', 'acme-112'])
-    assert defs.ProductId(__root__='acme-101') in pr_ids.__root__
+    pr_ids = product.Products(['acme-101', 'acme-112'])
+    assert defs.ProductId('acme-101') in pr_ids.root
     pr_name = product.FullProductName(name='wun', product_id=pr_id)
     data = {
         'category': product.RelationshipCategory.installed_with,
@@ -56,4 +56,4 @@ def test_product_relationship_dumps():
         'relates_to_product_reference': pr_ref_other,
     }
     rel = product.Relationship(**data)
-    assert msgspec.json.decode(rel.json()) == conftest.PRODUCT_RELATIONSHIP_DATA
+    assert msgspec.json.decode(rel.model_dump_json()) == conftest.PRODUCT_RELATIONSHIP_DATA
